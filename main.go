700708c2936e36e75eb6c09a2de7c8af4ad3ea4d
suapapa <Homin Lee>
2022-09-12 00:22:37 +0900

    clean up cache server code

--- main.go
+++ main.go
@@ -8,55 +8,108 @@ import (
 	"os"
 	"os/user"
 	"path/filepath"
-	"regexp"
 	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
 	"github.com/gokyle/filecache"
 )
 
-const VERSION = "3.0.1"
+func main() {
+	fExpire := flag.Int("e",
+		0, // filecache.DefaultExpireItem
+		"maximum number of seconds between accesses a file can stay in the cache")
+	fGarbage := flag.Int("g", filecache.DefaultEvery,
+		"scan the cache for expired items every <n> seconds")
+	fItems := flag.Int("n",
+		100, //filecache.DefaultMaxItems,
+		"max number of files to store in the cache")
+	fPort := flag.Int("p", 8080, "port to listen on")
+	fChroot := flag.Bool("r", false, "chroot to the working directory")
+	fSize := flag.Int64("s", filecache.DefaultMaxSize, "max file size to cache")
+	fUser := flag.String("u", "", "user to run as")
+	fSubPathRoot := flag.String("f", "", "remove prefix subpath in url.PATH")
+	fDumpCache := flag.String("d", "",
+		"dump cache stats duration; by default, this is turned off. Must be parsable with time.ParseDuration.")
+	flag.Parse()
 
-var handler func(http.ResponseWriter, *http.Request)
+	srvWD := "."
+	if flag.NArg() > 0 {
+		srvWD = flag.Arg(0)
+	}
+	if *fChroot {
+		srvWD = chroot(srvWD)
+	}
 
-var (
-	cache       *filecache.FileCache
-	path_regexp = regexp.MustCompile("^/(.*)$")
-	srv_bin     string
-)
+	if *fUser != "" {
+		setuid(*fUser)
+	}
 
-func init() {
-	cache = new(filecache.FileCache)
-	filecache.DefaultExpireItem = 0
-	handler = filecache.HttpHandler(cache)
+	srvWD, err := filepath.Abs(srvWD)
+	chk(err)
+	err = os.Chdir(srvWD)
+	chk(err)
+	cache := &filecache.FileCache{
+		MaxSize:    *fSize,
+		MaxItems:   *fItems,
+		ExpireItem: *fExpire,
+		Every:      *fGarbage,
+	}
+	cache.Start()
+
+	if *fDumpCache != "" {
+		go func() {
+			dur, err := time.ParseDuration(*fDumpCache)
+			if err != nil {
+				fmt.Printf("[-] couldn't parse %s: %v\n",
+					*fDumpCache, err)
+				return
+			}
+			tk := time.NewTicker(dur)
+			defer tk.Stop()
+			for range tk.C {
+				displayCacheStats(cache)
+			}
+		}()
+	}
+
+	srvAddr := fmt.Sprintf(":%d", *fPort)
+	fmt.Printf("serving %s on %s\n", srvWD, srvAddr)
+	http.HandleFunc("/",
+		func(w http.ResponseWriter, r *http.Request) {
+			if *fSubPathRoot != "" {
+				origURLPath := strings.Trim(r.URL.Path, "/")
+				origPaths := strings.Split(origURLPath, "/")
+				if len(origPaths) > 0 && origPaths[0] == strings.Trim(*fSubPathRoot, "/") {
+					r.URL.Path = "/" + strings.Join(origPaths[1:], "/")
+				}
+			}
+
+			filecache.HttpHandler(cache)(w, r)
+		},
+	)
+	if err := http.ListenAndServe(srvAddr, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func Path(r *http.Request) (path string) {
-	path = path_regexp.ReplaceAllString(r.URL.Path, "./$1")
-	if len(path) == 0 {
-		path = "."
-	}
-	return path
+func setuid(username string) {
+	usr, err := user.Lookup(username)
+	chk(err)
+	uid, err := strconv.Atoi(usr.Uid)
+	chk(err)
+	err = syscall.Setreuid(uid, uid)
+	chk(err)
 }
 
-func Dispatch(w http.ResponseWriter, r *http.Request) {
-	if r.URL.RawQuery == "cachestats" {
-		go displayCacheStats()
-	}
-	fi, err := os.Stat(Path(r))
-	if err != nil {
-		http.ServeFile(w, r, Path(r))
-	} else if fi.IsDir() {
-		http.ServeFile(w, r, Path(r))
-	} else if fi.Size() > cache.MaxSize {
-		http.ServeFile(w, r, Path(r))
-	} else {
-		cache.HttpWriteFile(w, r)
-	}
+func chroot(path string) string {
+	err := syscall.Chroot(path)
+	chk(err)
+	return "/"
 }
 
-func displayCacheStats() {
+func displayCacheStats(cache *filecache.FileCache) {
 	fmt.Printf("-----[ cache stats: %s ]-----\n",
 		time.Now().Format("2006-01-02 15:04:05"))
 	fmt.Printf("files cached: %d (max: %d)\n", cache.Size(),
@@ -71,117 +124,8 @@ func displayCacheStats() {
 	fmt.Printf("\n\n")
 }
 
-func main() {
-	var (
-		srv_port int
-		srv_ssl  bool
-		srv_wd   = "."
-	)
-
-	srv_bin = filepath.Base(os.Args[0])
-	empty := func(s string) bool {
-		return len(s) == 0
-	}
-
-	fCert := flag.String("c", "", "TLS certificate file")
-	fDumpCache := flag.String("d", "", "dump cache stats duration; by "+
-		"default, this is turned off. Must be parsable with "+
-		"time.ParseDuration.")
-	fExpire := flag.Int("e", filecache.DefaultExpireItem,
-		"maximum number of seconds between accesses a file can stay "+
-			"in the cache")
-	fGarbage := flag.Int("g", filecache.DefaultEvery,
-		"scan the cache for expired items every <n> seconds")
-	fKey := flag.String("k", "", "TLS key file")
-	fItems := flag.Int("n", filecache.DefaultMaxItems, "max number of "+
-		"files to store in the cache")
-	fPort := flag.Int("p", 8080, "port to listen on")
-	fChroot := flag.Bool("r", false, "chroot to the working directory")
-	fSize := flag.Int64("s", filecache.DefaultMaxSize,
-		"max file size to cache")
-	fUser := flag.String("u", "", "user to run as")
-	fVersion := flag.Bool("v", false, "print version information")
-	flag.Parse()
-
-	if *fVersion {
-		version()
-	}
-
-	if flag.NArg() > 0 {
-		srv_wd = flag.Arg(0)
-	}
-
-	if *fChroot {
-		srv_wd = chroot(srv_wd)
-	}
-
-	if !empty(*fUser) {
-		setuid(*fUser)
-	}
-
-	if !empty(*fCert) && !empty(*fKey) {
-		srv_ssl = true
-	}
-
-	if !(*fDumpCache == "") {
-		go func() {
-			dur, err := time.ParseDuration(*fDumpCache)
-			if err != nil {
-				fmt.Printf("[-] couldn't parse %s: %s\n",
-					*fDumpCache, err.Error())
-				return
-			}
-			for {
-				displayCacheStats()
-				<-time.After(dur)
-			}
-		}()
-	}
-
-	cache.MaxItems = *fItems
-	cache.ExpireItem = *fExpire
-	cache.Every = *fGarbage
-	cache.MaxSize = *fSize
-	srv_wd, err := filepath.Abs(srv_wd)
-	checkFatal(err)
-	err = os.Chdir(srv_wd)
-	checkFatal(err)
-	srv_port = *fPort
-	srv_addr := fmt.Sprintf(":%d", srv_port)
-	fmt.Printf("serving %s on %s\n", srv_wd, srv_addr)
-	cache.Start()
-	http.HandleFunc("/", handler)
-	cache.Start()
-	if srv_ssl {
-		log.Fatal(http.ListenAndServeTLS(srv_addr, *fCert, *fKey, nil))
-	} else {
-		log.Fatal(http.ListenAndServe(srv_addr, nil))
-	}
-}
-
-func checkFatal(err error) {
+func chk(err error) {
 	if err != nil {
-		fmt.Printf("[!] %s: %s\n", srv_bin, err.Error())
-		os.Exit(1)
+		log.Fatal(err)
 	}
-}
-
-func setuid(username string) {
-	usr, err := user.Lookup(username)
-	checkFatal(err)
-	uid, err := strconv.Atoi(usr.Uid)
-	checkFatal(err)
-	err = syscall.Setreuid(uid, uid)
-	checkFatal(err)
-}
-
-func chroot(path string) string {
-	err := syscall.Chroot(path)
-	checkFatal(err)
-	return "/"
-}
-
-func version() {
-	fmt.Printf("%s version %s\n", srv_bin, VERSION)
-	os.Exit(0)
 }

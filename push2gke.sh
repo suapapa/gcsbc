c525cc2bcefb003ed1934718dc1b1f4414a271bb
Homin Lee <ff4500@gmail.com>
2022-10-01 19:50:43 +0900

    add gke push script

---
+++ push2gke.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git tag -a $1 -m "add tag for $1"
+git push --tags
+
+IMAGE_TAG=gcr.io/homin-dev/gcsbc
+docker buildx build --platform linux/amd64 -t $IMAGE_TAG:$1 .
+docker push $IMAGE_TAG
+
+IMAGE_TAG_LATEST=$IMAGE_TAG:latest 
+docker tag $IMAGE_TAG $IMAGE_TAG_LATEST
+docker push $IMAGE_TAG_LATEST
